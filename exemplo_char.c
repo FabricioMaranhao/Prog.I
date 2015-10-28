4684ba8b4e33eb9d756614fd86ca2cca06fdbb87
fabricio_maranhao <fabriciosalles87@gmail.com>
2015-10-28 00:11:39 +0000

    exemplo de char

---
+++ exemplo_char.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    char nome[11];
+    
+    printf("\n Entre com um nome:\n");
+    scanf("%s", nome);
+    
+    printf("\n Boa Noite %s !!!", nome);
+    
+    return 0;
+}
